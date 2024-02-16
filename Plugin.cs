4ec6a4879a43bebca13ad6ae378484d87b99126c
Manuel Lucas <71901436+Duxi4@users.noreply.github.com>
2024-02-16 12:59:27 +0100

    Router ok en juego a falta de texturas
    El router esta bien de posicion en la mano y lo demás. Falta aplicarle la textura ya que la forma en la que tiene la textura ahora hace que el logo de digi se vea sin tener el obj en la mano.

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,28 @@
 ﻿using BepInEx;
+using LethalLib.Modules;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
 
 namespace DigiMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)] 
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
             // Plugin startup logic
+            int iRarity = 45;
+            string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "digimod.asset");
+            AssetBundle bundle = AssetBundle.LoadFromFile(assetDir);
+
+            Item digiRouter = bundle.LoadAsset<Item>("Assets/router.asset");
+            NetworkPrefabs.RegisterNetworkPrefab(digiRouter.spawnPrefab);
+            Utilities.FixMixerGroups(digiRouter.spawnPrefab);
+            Items.RegisterScrap(digiRouter, iRarity, Levels.LevelTypes.All);
+
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DigiMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "DigiMod";
+        public const string PLUGIN_NAME = "DigiMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
